e71087494940e2eb66d8862abc314026fdc30fb6
hyurl <i@hyurl.com>
2018-09-03 01:31:52 +0800

    remove used files

--- dist/src/core/tools/symbols.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.realDB = Symbol("realDB");
-exports.realSSE = Symbol("realSSE");
-exports.realCsrfToken = Symbol("realCsrfToken");
-//# sourceMappingURL=symbols.js.map
