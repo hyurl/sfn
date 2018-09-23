c6dc7ee9769b5e9f9c2b988527eedc635783178a
hyurl <i@hyurl.com>
2018-09-24 01:19:56 +0800

    update dependency

--- dist/core/handlers/worker/http-session.js
+++ dist/core/handlers/worker/http-session.js
@@ -1,19 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ExpressSession = require("express-session");
+const function_intercepter_1 = require("function-intercepter");
 const ConfigLoader_1 = require("../../bootstrap/ConfigLoader");
 const index_1 = require("../../bootstrap/index");
 exports.session = ExpressSession(ConfigLoader_1.config.session);
 index_1.app.use(exports.session).use((req, res, next) => {
-    let _end = res.end;
-    res.end = function end(...args) {
+    res.sent = false;
+    res.end = function_intercepter_1.intercept(res.end).before(() => {
         res.sent = true;
-        if (req.httpVersion == "2.0") {
-            res.statusMessage = "";
-            res.removeHeader("connection");
-        }
-        _end.apply(res, args);
-    };
+    });
     next();
 });
 //# sourceMappingURL=http-session.js.map
--- src/core/handlers/worker/http-session.ts
+++ src/core/handlers/worker/http-session.ts
@@ -1,4 +1,5 @@
 import * as ExpressSession from "express-session";
+import { intercept } from "function-intercepter";
 import { config } from "../../bootstrap/ConfigLoader";
 import { app } from "../../bootstrap/index";
 import { Response } from "../../tools/interfaces";
@@ -8,17 +9,9 @@ type SessionHanlder = (req: any, res: any, next: Function) => void;
 export var session = <SessionHanlder>ExpressSession(config.session);
 
 app.use(session).use((req, res: Response, next) => {
-    let _end = res.end;
-    res.end = function end(...args) {
+    res.sent = false;
+    res.end = intercept(res.end).before(() => {
         res.sent = true;
-
-        // HTTP/2 disallow setting these headers.
-        if (req.httpVersion == "2.0") {
-            res.statusMessage = "";
-            res.removeHeader("connection");
-        }
-
-        _end.apply(res, args);
-    }
+    });
     next();
 });
