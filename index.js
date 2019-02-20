ccf8c955f846e0a252b6abf67d64ca0c02beb67c
Ayon Lee <i@hyurl.com>
2019-02-21 00:12:50 +0800

    reset entry

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+let isTsNode = process.execArgv.join(" ").includes("ts-node");
+let entry = path.dirname(process.mainModule.filename);
+
+if (isTsNode && entry === path.resolve(__dirname, "src")) {
+    module.exports = require("./src");
+} else {
+    module.exports = require("./dist");
+}
