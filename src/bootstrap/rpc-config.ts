1ad35696445d3a151c20b44a355d5b8739aa9502
Ayon Lee <i@hyurl.com>
2019-04-06 02:08:21 +0800

    change rpc server port, update dependencies

--- src/bootstrap/rpc-config.ts
+++ src/bootstrap/rpc-config.ts
@@ -3,12 +3,12 @@ import "sfn";
 app.config.server.rpc = {
     "doc-server": {
         host: "localhost",
-        port: 8000,
+        port: 4001,
         modules: [app.services.docs, app.services.test]
     },
     "schedule-server": {
         host: "localhost",
-        port: 8001,
+        port: 4002,
         modules: [app.services.schedule]
     }
 };
