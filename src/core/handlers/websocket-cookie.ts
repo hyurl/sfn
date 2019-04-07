5955216e1f11eb152e444b8094f0873d35672848
Ayon Lee <i@hyurl.com>
2019-04-07 23:26:29 +0800

    update dependencies, remove unused scripts from npm package

--- src/core/handlers/websocket-cookie.ts
+++ src/core/handlers/websocket-cookie.ts
@@ -2,10 +2,10 @@ import cookieParser = require("cookie-parser");
 import { config } from "../bootstrap/load-config";
 import { WebSocket } from "../tools/interfaces";
 
-const parser = cookieParser(config.session.secret);
+const parse = cookieParser(config.session.secret);
 
 export default async function (socket: WebSocket, next: (err?: Error) => void) {
-    parser(<any>socket.handshake, <any>{}, next);
+    parse(<any>socket.handshake, <any>{}, next);
 }
 
 export async function handler2(socket: WebSocket, next: (err?: Error) => void) {
