e518f337b0ec56fe064f6bd6ca3d0f70c531b68d
Ayon Lee <ayonlys@gmail.com>
2019-08-19 12:10:53 +0800

    fix entry

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require("./env-fix");
-const path = require("path");
 
 let isTsNode = process.execArgv.join(" ").includes("ts-node");
-let entry = path.dirname(process.mainModule.filename);
 
-if (isTsNode && entry === path.resolve(__dirname, "src")) {
+if (isTsNode) {
     module.exports = require("./src");
 } else {
     module.exports = require("./dist");
