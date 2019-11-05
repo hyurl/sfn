42b86affd91990d63fb0d699e7c59110b962222e
Ayon Lee <ayonlys@gmail.com>
2019-11-05 12:23:44 +0800

    fix a bug

--- src/core/tools/internal/error.ts
+++ src/core/tools/internal/error.ts
@@ -89,7 +89,7 @@ export async function tryLogError(err: any, stack?: string) {
         if (stack) {
             stack = `[${stack}]`;
         } else {
-            stack = (<Error>err).stack.split("\n")[1].trim();
+            stack = (<Error>err).stack.split("\n")[0].trim();
         }
 
         console.log(red`${err.toString()} ${stack}`);
